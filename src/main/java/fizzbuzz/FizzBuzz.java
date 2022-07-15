19e01262c17470f6357e605c67b642109a034236
Virjanand Panday <virjanand@virjanand.com>
2022-07-15 15:33:13 +0200

    Extract to methods fizzRule and buzzRule

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -8,17 +8,25 @@ import java.util.stream.Stream;
 
 public class FizzBuzz {
     public List<String> getNumbers(int from, int until) {
-        return IntStream.rangeClosed(from, until).mapToObj(number -> toFizzBuzz(number, Stream.of(() -> {
+        return IntStream.rangeClosed(from, until).mapToObj(number -> toFizzBuzz(number, Stream.of(fizzRule(number), buzzRule(number)))).collect(Collectors.toList());
+    }
+
+    private Supplier<String> fizzRule(int number) {
+        return () -> {
             if (number % 3 == 0) {
                 return "Fizz";
             }
             return "";
-        }, () -> {
+        };
+    }
+
+    private Supplier<String> buzzRule(int number) {
+        return () -> {
             if (number % 5 == 0) {
                 return "Buzz";
             }
             return "";
-        }))).collect(Collectors.toList());
+        };
     }
 
     private String toFizzBuzz(int number, Stream<Supplier<String>> rules) {
