046e97ab6da49037bab5fede2a258fc7392c28b9
Virjanand Panday <virjanand@virjanand.com>
2022-07-15 15:30:21 +0200

    Extract parameter buzzRule and convert to Supplier

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -1,7 +1,6 @@
 package fizzbuzz;
 
 import java.util.List;
-import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -13,14 +12,17 @@ public class FizzBuzz {
                 return "Fizz";
             }
             return "";
+        }, () -> {
+            if (number % 5 == 0) {
+                return "Buzz";
+            }
+            return "";
         })).collect(Collectors.toList());
     }
 
-    private String toFizzBuzz(int number, Supplier<String> fizzRule) {
+    private String toFizzBuzz(int number, Supplier<String> fizzRule, Supplier<String> buzzRule) {
         String result = fizzRule.get();
-        if (number % 5 == 0) {
-            result += "Buzz";
-        }
+        result += buzzRule.get();
         if (result.isEmpty()) {
             return Integer.toString(number);
         }
